4c51677943e6710ce7fc17e12043318c46fb3b1a
Rowhnn <rohanatozayadav@gmail.com>
2025-10-04 17:25:45 +0530

    Updated

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+
 const port = 3000;
 const https = require("https");
 
@@ -8,7 +9,7 @@ const mailchimpApiKey = process.env.MAILCHIMP_API_KEY;
 const listId = process.env.MAILCHIMP_LIST_ID;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public")); // access static files via relative URLs
+app.use(express.static("public")); //makes me able to access to  static file through relative url
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signup.html");
@@ -19,11 +20,11 @@ app.listen(port, () => {
 });
 
 app.post("/", (req, res) => {
+  // res.send("Posted successfully.");
   const firstName = req.body.fname;
   const lastName = req.body.lname;
   const email = req.body.email;
   console.log(firstName, lastName, email);
-
   const data = {
     members: [
       {
@@ -37,16 +38,11 @@ app.post("/", (req, res) => {
     ],
   };
   const jsonData = JSON.stringify(data);
-  const url = `https://us4.api.mailchimp.com/3.0/lists/${listId}`;
+  const url = "https://us4.api.mailchimp.com/3.0/lists/ce2129b249";
   const options = {
     method: "POST",
-    auth: `rohan9:${mailchimpApiKey}`,
-    headers: {
-      "Content-Type": "application/json",
-      "Content-Length": Buffer.byteLength(jsonData),
-    },
+    auth: "rohan9:5a5c5a9a51aaffc60423c1a7d3b385f1-us4",
   };
-
   const request = https.request(url, options, function (response) {
     if (response.statusCode === 200) {
       res.sendFile(__dirname + "/success.html");
@@ -58,7 +54,6 @@ app.post("/", (req, res) => {
       console.log(JSON.parse(data));
     });
   });
-
   request.write(jsonData);
   request.end();
 });
@@ -66,3 +61,9 @@ app.post("/", (req, res) => {
 app.post("/failure", (req, res) => {
   res.redirect("/");
 });
+
+// curl -X POST \
+// "","address1":"","address2":"","city":"","state":"","zip":"","country":"","phone":""},"permission_reminder":"","use_archive_bar":false,"campaign_defaults":{"from_name":"","from_email":"","subject":"","language":""},"notify_on_subscribe":"","notify_on_unsubscribe":"","email_type_option":false,"double_optin":false,"marketing_permissions":false}'
+// // {"name":"","contact":{"company":"","address1":"","address2":"","city":"","state":"","zip":"","country":"","phone":""},"permission_reminder":"","use_archive_bar":false,"campaign_defaults":{"from_name":"","from_email":"","subject":"","language":""},"notify_on_subscribe":"","notify_on_unsubscribe":"","email_type_option":false,"double_optin":false,"marketing_permissions":false}//   https://${dc}.api.mailchimp.com/3.0/lists \
+// //   --user "anystring:${apikey}"' \
+// //   -d '{"name":"","contact":{"company":
